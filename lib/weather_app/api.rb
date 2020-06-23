57863efed9466b3e7222d6ee8566a08468ea843b
chienleow <chienleow1@gmail.com>
2020-06-22 22:49:11 -0400

    commit .

--- lib/weather_app/api.rb
+++ lib/weather_app/api.rb
@@ -14,17 +14,37 @@ class WeatherApp::API
     #     end
     # end
 
-    def self.valid_api_response
+    def self.get_weather_five_days
         key = "d11a067cc49844d662653dd873932b94"
-        city = "Wwilmore"
+        city = "Wwwwilmore"
         state_code ="KY"
         response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{city},#{state_code},US&units=imperial&appid=#{key}")
         if response.has_value?("city not found")
-            puts "\ERROR: Invalid city name, please enter again.".colorize(:red)
+            puts "\ERROR: Invalid city name, please try again.".colorize(:red)
         else
             main_weather = response["list"][0]["weather"].first["main"]
             temperature = response["list"][0]["main"]["temp"].to_s
             WeatherApp::Weather.new(main_weather, temperature)
         end
     end
+
+    def self.valid_response
+        key = "d11a067cc49844d662653dd873932b94"
+        city = "Wwwwilmore"
+        state_code ="KY"
+        response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{city},#{state_code},US&units=imperial&appid=#{key}")
+    end
+
+
+
+
+
+    # def valid_city_api_response
+    #     response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?q=#{city},#{state_code},US&units=imperial&appid=#{key}")
+    #     if !response.has_value?("city not found")
+    #         response
+    #     else
+    #     end
+    # end
+
 end
--- lib/weather_app/cli.rb
+++ lib/weather_app/cli.rb
@@ -11,16 +11,30 @@ class WeatherApp::CLI
         exit
     end
 
-    def invalid_api_response
-        puts "\Invalid city name, please enter again." #try zipcode?
-    end
+    # def invalid_api_response
+    #     puts "\Invalid city name, please enter again." #try zipcode?
+    # end
 
     def enter_location
         puts "\Please enter the city you want to search for."
     end
 
     def get_user_input
+        user_input = gets.strip # caps or not doesn't affect the result
+        until valid_city_input(user_input)
+            invalid_input
+            user_input = gets.strip
+        end
+    end
 
+    def valid_city_input(user_input)
+        if !WeatherApp::API.valid_response.has_value?("city not found")
+            user_input
+        end
+    end
+
+    def invalid_city_input
+        puts "\ERROR: Invalid city name, please try again.".colorize(:red)
     end
 
     def verify_location
@@ -35,10 +49,12 @@ class WeatherApp::CLI
     end
 
     def next_step
-
+        puts "\nType 'exit' to quit app or hit any key to choose a new category.".colorize(:yellow)
+        user_input = gets.strip
     end
 
     def exit
-
+        puts "\nGoodbye, thanks for using the app!"
+        puts "\nWherever you go, no matter what the weather, always bring your own sunshine. -Anthony J.D'Angelo"
     end
 end
