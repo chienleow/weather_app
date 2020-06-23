ce26d163b2af0d03bfa67f7653df16e2466ec77b
chienleow <chienleow1@gmail.com>
2020-06-23 15:03:08 -0400

    commit .

--- lib/weather_app/api.rb
+++ lib/weather_app/api.rb
@@ -5,24 +5,38 @@ class WeatherApp::API
         zipcode = user_input
         country = "US"
         api_key = ENV['OPEN_WEATHER_MAP_API_KEY']
-        # response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?zip=#{zipcode},#{country}&units=imperial&mode=xml&appid=#{api_key}") # XML
+        response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?zip=#{zipcode},#{country}&units=imperial&mode=xml&appid=#{api_key}") # XML
         # binding.pry
         # if response.has_value?("404")
         #     WeatherApp::CLI.new.invalid_input
         #     WeatherApp::CLI.new.get_user_input
         # else
-        #     city = response["name"]
+            # city = response["weatherdata"]["location"]["name"]
+            data = response["weatherdata"]["forecast"]["time"]
+            data.each do |item|
+                main_weather = item["symbol"]["name"]
+                time_from = item["from"]
+                WeatherApp::Weather.new(main_weather, time_from)
+            end
+            # main_weather = response["weatherdata"]["forecast"]["time"][0]["symbol"]["name"]
+            # time_from = response["weatherdata"]["forecast"]["time"][0]["from"]
+            # binding.pry
+            # end
+        # end
+    end
+end
+
+
+
+# city = response["name"]
         #     main_weather = response["weather"][0]["main"]
         #     temperature = response["main"]["temp"].to_s
         #     WeatherApp::Weather.new(city, main_weather, temperature)
-            # city = response["weatherdata"]["location"]["name"] ## 5 day forecast XML
-            # time_from = response["weatherdata"]["forecast"]["time"][0]["from"] 5 day forecast XML
 
 
-        # end
 
-        response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?zip=55102,US&units=imperial&appid=d11a067cc49844d662653dd873932b94") # JSON
-        binding.pry
-        time = response["list"][0]["dt_txt"]
-    end
-end
+
+        # response = HTTParty.get("http://api.openweathermap.org/data/2.5/forecast?zip=55102,US&units=imperial&appid=d11a067cc49844d662653dd873932b94") # JSON
+        # binding.pry
+        # data = response["list"]
+        # time = response["list"][0]["dt_txt"]
